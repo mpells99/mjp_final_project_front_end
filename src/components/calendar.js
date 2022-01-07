df4cf02d7223576a381ffaf680741de05eeb3466
MJ Pells <mcdonaldjpells@gmail.com>
2022-01-06 20:57:45 -0700

    fixed poppop feature and enabled date to be shown in popped window

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
-import Popped from "./popup-box";
+import PropTypes from "prop-types";
+import PopPop from "react-poppop";
 
 export function CalendarJS() {
   const [value, onChange] = useState(new Date());
+  const [showPop, setShowPop] = useState(false);
 
   return (
     <div>
       <Calendar
         onChange={onChange}
         value={value}
-        onClickDay={console.log("You clicked on: " + value)}
+        onClickDay={() => {
+          setShowPop(true);
+          console.log("You clicked on: " + value);
+        }}
       />
-      <Popped onClick={open} />
+      <PopPop
+        position="centerCenter"
+        open={showPop}
+        closeBtn={true}
+        closeOnEsc={true}
+        onClose={() => setShowPop(false)}
+        closeOnOverlay={true}
+      >
+        <h1>{value.toDateString()}</h1>
+        <p>
+          content
+          {/* server info about booking */}
+        </p>
+      </PopPop>
     </div>
   );
 }
